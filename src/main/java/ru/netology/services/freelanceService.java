c5b5822692237cc2415dbae0a031dbf2e10c0f15
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-05-12 12:15:57 +0400

    Initial commit

---
+++ src/main/java/ru/netology/services/freelanceService.java
@@ -0,0 +1,24 @@
+package ru.netology.services;
+
+public class freelanceService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int balance = 0;
+        int monthofvacantion = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (balance >= threshold) {
+                monthofvacantion++;
+                balance = (balance - expenses) / 3;
+            } else {
+                balance = balance + income - expenses;
+
+            }
+        }
+return monthofvacantion;
+    }
+
+}
+
+
+
---
+++ src/test/java/ru/netology/services/freelanceServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.services;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class freelanceServiceTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+
+    public void shouldCalcFreelance(int expected, int income, int expenses, int threshold) {
+        freelanceService service = new freelanceService();
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
