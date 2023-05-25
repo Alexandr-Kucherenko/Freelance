702e5e5b50b2b5452be4c8a233e2703fb88b9d60
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-05-25 17:33:28 +0400

    Deleting the resources file and correcting the name

--- src/main/java/ru/netology/services/freelanceService.java
+++ src/main/java/ru/netology/services/freelanceService.java
@@ -4,18 +4,18 @@ public class freelanceService {
 
     public int calculate(int income, int expenses, int threshold) {
         int balance = 0;
-        int monthofvacantion = 0;
+        int monthOfVacantion = 0;
 
         for (int i = 0; i < 12; i++) {
             if (balance >= threshold) {
-                monthofvacantion++;
+                monthOfVacantion++;
                 balance = (balance - expenses) / 3;
             } else {
                 balance = balance + income - expenses;
 
             }
         }
-        return monthofvacantion;
+        return monthOfVacantion;
     }
 
 }
